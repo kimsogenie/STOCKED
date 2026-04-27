4611b3630e7ebe7ad5c342ad235bce4d593c0161
kimsogenie <kimsogenie@gmail.com>
2026-04-27 18:07:20 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['search1.kakaocdn.net', 'thumbnail.image.kakao.com'],
+  },
+}
+
+module.exports = nextConfig
