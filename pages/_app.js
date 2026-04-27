6ce6d98484ba71e1ae73cc91b1b55092bd8326f0
kimsogenie <kimsogenie@gmail.com>
2026-04-27 23:29:55 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>STOCKED</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="나의 책장과 명대사 영수증" />
+        <link rel="icon" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
