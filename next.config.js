0f7a077e8c7d8c63026e03edc7f62ceb592aeedb
kimsogenie <kimsogenie@gmail.com>
2026-05-02 01:08:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  },
   images: {
     domains: ['search1.kakaocdn.net', 'thumbnail.image.kakao.com'],
   },
